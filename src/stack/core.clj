b0813aced64f4acc8335341680988999f23f0eea
d11wtq <chris@w3style.co.uk>
2015-03-08 12:20:13 +0000

    Support --help on -main function.
    Uses parse-opts to check options. Implicitly dispatches to :help without
    arguments.

--- src/stack/core.clj
+++ src/stack/core.clj
@@ -1,20 +1,34 @@
 (ns stack.core
-  (:require [stack.util :as util]
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [stack.util :as util]
             [stack.wiring.commands.deploy :as deploy]
             [stack.wiring.commands.help :as help]))
 
 (def flags
+  "Supported command line flags"
   [["-h" "--help"
     "Show this usage info"]])
+
+(defn implicit-help?
+  [arguments options]
+  (or (:help options)
+      (empty? arguments)))
 
 (defn make-dispatch-fn
   "Return a fn accepting ([& args]) and dispatching to a subcommand like git."
   [commands error-fn]
   (fn [& args]
-    (let [[cmd-name & remaining] args]
-      (if-let [dispatch-fn (get commands (keyword cmd-name))]
-        (apply dispatch-fn remaining)
-        (error-fn (str "Unknown command: " cmd-name))))))
+    (let [{:keys [arguments options errors]} (parse-opts (take 1 args) flags)
+          cmd-name (first arguments)]
+      (if (implicit-help? arguments options)
+        (recur ["help"])
+        (try
+          (if-let [dispatch-fn (get commands (keyword cmd-name))]
+            (apply dispatch-fn (drop 1 args))
+            (error-fn (str "Unknown command: " cmd-name)))
+          (catch Exception e
+            (binding [*out* *err*] (println (.getMessage e)))
+            (System/exit 1)))))))
 
 (def -main
   (make-dispatch-fn
