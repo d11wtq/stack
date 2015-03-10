9a80d91f1399c5b12fc718452962aa6da7bd38f1
d11wtq <chris@w3style.co.uk>
2015-03-10 10:40:44 +0000

    Implement basic stack deploy functionality.
    Currently accepts no parameters, but validates that stack-name and
    template are provided before applying the injected deploy-fn.

--- src/stack/commands/deploy.clj
+++ src/stack/commands/deploy.clj
@@ -1,4 +1,5 @@
-(ns stack.commands.deploy)
+(ns stack.commands.deploy
+  (:require [clojure.data.json :as json]))
 
 (def flags
   "Supported command line flags"
@@ -9,10 +10,44 @@
 
 (defn usage
   [summary]
-  summary)
+  (str "Usage: stack deploy <stack-name> <template> [opts...] [key=value...]"
+       \newline
+       \newline
+       "Options:"
+       \newline
+       summary))
+
+(defn validate-stack-name
+  [[stack-name] options]
+  (if (nil? stack-name)
+    "<stack-name> required"))
+
+(defn validate-template
+  [[anything template] options]
+  (if (nil? template)
+    "<template> required"))
+
+(defn validate-all
+  [arguments options]
+  (letfn [(validate [f]
+            (f arguments options))]
+    (some validate
+          [validate-stack-name
+           validate-template])))
+
+(defn slurp-json
+  [path]
+  (json/read-str (slurp path)
+                 :key-fn keyword))
 
 (defn action
-  [{:keys [deploy-fn error-fn]} arguments options])
+  [{:keys [deploy-fn error-fn]} arguments options]
+  (if-let [msg (validate-all arguments options)]
+    (error-fn msg)
+    (let [[stack-name template] arguments]
+      (deploy-fn stack-name
+                 (slurp-json template)
+                 (hash-map)))))
 
 (defn dispatch
   [{:keys [parse-fn handler-fn]} & args]
--- test/stack/commands/deploy_test.clj
+++ test/stack/commands/deploy_test.clj
@@ -1,4 +1,66 @@
 (ns stack.commands.deploy-test
   (:require [clojure.test :refer :all]
             [bond.james :as bond]
+            [clojure.data.json :as json]
             [stack.commands.deploy :as deploy]))
+
+(def sample-template-path
+  "tmp/sample-template.json")
+
+(def sample-template
+  {:AWSTemplateFormatVersion "2010-09-09"
+   :Description "Example"
+   :Parameters {:dockerImage {:Type "String"}
+                :hostPort {:Type "Number"}}})
+
+(use-fixtures
+  :once
+  (fn [f]
+    (spit sample-template-path
+          (json/write-str sample-template))
+
+    (f)))
+
+(deftest action-test
+  (testing "#'action"
+    (testing "without a <stack-name>"
+      (testing "applies error-fn"
+        (let [error-fn (bond/spy (constantly nil))
+              deploy-fn (constantly nil)]
+          (deploy/action {:error-fn error-fn
+                          :deploy-fn deploy-fn}
+                         (vector)
+                         (hash-map))
+          (is (re-find #"<stack-name>"
+                       (-> (bond/calls error-fn)
+                           first
+                           :args
+                           first))))))
+
+    (testing "without a <template>"
+      (testing "applies error-fn"
+        (let [error-fn (bond/spy (constantly nil))
+              deploy-fn (constantly nil)]
+          (deploy/action {:error-fn error-fn
+                          :deploy-fn deploy-fn}
+                         ["example-stack"]
+                         (hash-map))
+          (is (re-find #"<template>"
+                       (-> (bond/calls error-fn)
+                           first
+                           :args
+                           first))))))
+
+    (testing "with a <stack-name> and <template>"
+      (testing "without --params"
+        (testing "applies deploy-fn with empty params"
+          (let [error-fn (constantly nil)
+                deploy-fn (bond/spy (constantly nil))]
+            (deploy/action {:error-fn error-fn
+                            :deploy-fn deploy-fn}
+                           ["example-stack" sample-template-path]
+                           (hash-map))
+            (is (= (-> (bond/calls deploy-fn) first :args)
+                   ["example-stack"
+                    sample-template
+                    (hash-map)]))))))))
